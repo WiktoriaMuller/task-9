47f4b2b20041c9210fbf7ed8a0636110809c1f93
Wiktoria Muller <wwiktoriamuller@gmail.com>
2021-05-22 19:25:00 +0200

    Done

--- js/my.js
+++ js/my.js
@@ -1,8 +1,16 @@
 // Create variables (fullName, phone, hobbies, email, websiteLink, websiteTitle) after this comment
 // what type should you use if this variables will not be changed?
 
+const fullName = 'Wiktoria Muller';
+const phone = false;
+const hobbies = 'psychology, graphic design, music';
+const email = 'wiktoriamuller@gmail.com';
+const websiteLink = 'www.google.com';
+const websiteTitle = 'Google';
+
+
 /**
  *  Remove '//' before render(.... to uncomment function and make it work
  */
 
-//  render(fullName, phone, hobbies, email, websiteLink, websiteTitle);
+render(fullName, phone, hobbies, email, websiteLink, websiteTitle);
